2fb7868892bf08e1e877aaad164cee92fb363bb4
FarciarzYT <140611697+FarciarzYT@users.noreply.github.com>
2026-04-20 18:53:47 +0200

    winter group update date for 2027

--- src/data/WinterBreakGroups.ts
+++ src/data/WinterBreakGroups.ts
@@ -10,22 +10,22 @@ export const winterBreakGroups: WinterBreakGroup[] = [
     {
         id: 'winter-group-1',
         label: 'ferii',
-        date: new Date('2026-01-19T00:00:00'),
-        title: '19.01 - 01.02.2026',
-        provinces: 'mazowieckie, pomorskie, podlaskie, świętokrzyskie, warmińsko-mazurskie'
+        date: new Date('2027-01-18T00:00:00'),
+        title: '18.01 - 31.01.2027',
+        provinces: 'podkarpackie, podlaskie, dolnośląskie, łódzkie, śląskie, opolskie'
     },
     {
         id: 'winter-group-2',
         label: 'ferii',
-        date: new Date('2026-01-26T00:00:00'),
-        title: '26.01 - 08.02.2026',
-        provinces: 'dolnośląskie, kujawsko-pomorskie, łódzkie, zachodniopomorskie, małopolskie, opolskie'
+        date: new Date('2027-02-01T00:00:00'),
+        title: '01.02 - 14.02.2027',
+        provinces: 'mazowieckie, pomorskie, świętokrzyskie, lubelskie'
     },
     {
         id: 'winter-group-3',
         label: 'ferii',
-        date: new Date('2026-02-16T00:00:00'),
-        title: '16.02 - 01.03.2026',
-        provinces: 'podkarpackie, lubelskie, wielkopolskie, lubuskie, śląskie'
+        date: new Date('2027-02-15T00:00:00'),
+        title: '15.02 - 28.02.2027',
+        provinces: 'lubuskie, kujawsko-pomorskie, warmińsko-mazurskie, wielkopolskie, zachodniopomorskie, małopolskie'
     }
 ];
